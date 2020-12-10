219f4b077efb4b177b7a7a870dac18ea3787ab59
theo <teddyhoover@gmail.com>
2020-12-10 13:45:46 -0600

    commit tree 8f69a1132ec3d699cd8bc63cff7fce78415e4ee9

--- prime.rb
+++ prime.rb
@@ -1,1 +1,36 @@
 # Add  code here!
+def prime? (number)
+
+    i = 1
+    number = number * -1 if number < 0
+    prime = true
+
+
+    if number == 1 || number == 0
+        prime = false
+    elsif number == 2
+        prime = true
+    else
+        while i < number / 2 && prime == true
+            i += 1
+            if number % i == 0
+                puts "Number: #{number} / #{i}"
+                puts "Remainder is zero"
+                prime = false
+            end
+            
+        end
+    end
+
+=begin    
+    else
+        while i < number
+            i++
+            if number % i == 0
+                prime = false 
+            end
+        end
+    end
+=end
+    prime
+end
